4d0884746f2554b9e92787b7ab4ad47c23b50431
Andre Santana <andrelusantana@hotmail.com>
2022-04-21 20:55:24 -0300

    Primeira implemntacao TecnicoService findById

---
+++ src/main/java/com/andre/helpdesk/controllers/TecnicoController.java
@@ -0,0 +1,5 @@
+package com.andre.helpdesk.controllers;
+
+public class TecnicoController {
+
+}
---
+++ src/main/java/com/andre/helpdesk/services/TecnicoService.java
@@ -0,0 +1,23 @@
+package com.andre.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andre.helpdesk.domain.Tecnico;
+import com.andre.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+	
+	@Autowired
+	TecnicoRepository tecnicoRepository;
+	
+	public Tecnico findById(Long id) {
+		
+		Optional<Tecnico> obj = tecnicoRepository.findById(id);
+		return obj.orElse(null);
+	}
+
+}
