246abf4ceab8547094510af427318a522c9b7ab8
Andre Santana <andrelusantana@hotmail.com>
2022-04-21 21:02:19 -0300

    Primeira implementacao TecnicoController findById

--- src/main/java/com/andre/helpdesk/controllers/TecnicoController.java
+++ src/main/java/com/andre/helpdesk/controllers/TecnicoController.java
@@ -1,5 +1,24 @@
 package com.andre.helpdesk.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andre.helpdesk.domain.Tecnico;
+import com.andre.helpdesk.services.TecnicoService;
+
+@RestController
+@RequestMapping(value="/tecnico")
 public class TecnicoController {
 
+	@Autowired
+	TecnicoService tecnicoService;
+	
+	public ResponseEntity<Tecnico> findById(@PathVariable Long id){
+		
+		Tecnico obj = tecnicoService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
