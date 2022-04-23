46d82d256caac5d5bc920dbb78440498def531d8
Andre Santana <andrelusantana@hotmail.com>
2022-04-23 11:43:05 -0300

    Crĩação metodos findByCpf e findByEmail

--- src/main/java/com/andre/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/andre/helpdesk/repositories/PessoaRepository.java
@@ -1,10 +1,13 @@
 package com.andre.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.andre.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
 	
-
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
