de50d7a3794dbdcd9e8f3774e254d0fa8be88540
Andre Santana <andrelusantana@hotmail.com>
2022-04-23 11:38:05 -0300

    Inclusao metodos exceptions handler

--- src/main/java/com/andre/helpdesk/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/andre/helpdesk/controllers/exceptions/ResourceExceptionHandler.java
@@ -9,7 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.andre.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.andre.helpdesk.services.exceptions.ObjectNotFoundException;
+import com.andre.helpdesk.services.exceptions.StringIndexOutOfBoundsException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -24,4 +26,23 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.ok().body(err);
 	}
 	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request){
+		
+		String error = "Violação de dados";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.ok().body(err);
+	}
+	
+	@ExceptionHandler(StringIndexOutOfBoundsException.class)
+	public ResponseEntity<StandardError> stringIndexOutOfBoundsException(StringIndexOutOfBoundsException ex, HttpServletRequest request){
+		
+		String error = "String com tamanho errado.";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.ok().body(err);
+	}
 }
