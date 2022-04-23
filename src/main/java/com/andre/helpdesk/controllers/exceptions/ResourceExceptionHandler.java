ffea4279519eaec48c38c382a927fbd8bd363cce
Andre Santana <andrelusantana@hotmail.com>
2022-04-23 08:01:45 -0300

    Criacao StandError e ResourceExceptionHandler

---
+++ src/main/java/com/andre/helpdesk/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.andre.helpdesk.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.andre.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ObjectNotFoundException ex, HttpServletRequest request){
+		
+		String error = "Object not found!";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.ok().body(err);
+	}
+	
+}
