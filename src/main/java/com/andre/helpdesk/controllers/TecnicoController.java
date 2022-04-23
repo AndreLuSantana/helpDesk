c85c0e120f9fa3200f7dca254e79f7882cca86bb
Andre Santana <andrelusantana@hotmail.com>
2022-04-23 09:22:06 -0300

    Criacao metodo create TecnicoController

--- src/main/java/com/andre/helpdesk/controllers/TecnicoController.java
+++ src/main/java/com/andre/helpdesk/controllers/TecnicoController.java
@@ -1,13 +1,17 @@
 package com.andre.helpdesk.controllers;
 
+import java.net.URI;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andre.helpdesk.domain.Tecnico;
 import com.andre.helpdesk.domain.dtos.TecnicoDTO;
@@ -33,4 +37,10 @@ public class TecnicoController {
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 	
+	@PostMapping
+	public ResponseEntity<TecnicoDTO> create(@RequestBody Tecnico tecnico){
+		TecnicoDTO tecnicoDTO = tecnicoService.create(tecnico);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(tecnicoDTO.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
