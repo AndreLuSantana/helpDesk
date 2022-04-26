e408fe2db92ccec321e1ee3c5970cdc76f7890cf
Andre Santana <andrelusantana@hotmail.com>
2022-04-26 15:46:59 -0300

    Ajustes para o pleno funcionamento da funçionalidade delete

--- src/main/java/com/andre/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/andre/helpdesk/HelpdeskApplication.java
@@ -1,21 +1,7 @@
 package com.andre.helpdesk;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.andre.helpdesk.domain.Chamado;
-import com.andre.helpdesk.domain.Cliente;
-import com.andre.helpdesk.domain.Tecnico;
-import com.andre.helpdesk.domain.enums.Perfil;
-import com.andre.helpdesk.domain.enums.Prioridade;
-import com.andre.helpdesk.domain.enums.Status;
-import com.andre.helpdesk.repositories.ChamadoRepository;
-import com.andre.helpdesk.repositories.ClienteRepository;
-import com.andre.helpdesk.repositories.TecnicoRepository;
 
 @SpringBootApplication
 public class HelpdeskApplication{
