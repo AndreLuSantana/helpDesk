afead09e6941cb5cf8f4f0180ccae0d17b7a416b
Andre Santana <andrelusantana@hotmail.com>
2022-04-27 14:30:57 -0300

    Criação método findById e create ClienteService

--- src/main/java/com/andre/helpdesk/services/ClienteService.java
+++ src/main/java/com/andre/helpdesk/services/ClienteService.java
@@ -1,6 +1,7 @@
 package com.andre.helpdesk.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.andre.helpdesk.domain.Cliente;
 import com.andre.helpdesk.domain.dtos.ClienteDTO;
 import com.andre.helpdesk.repositories.ClienteRepository;
+import com.andre.helpdesk.services.exceptions.ObjectNotFoundException;
 import com.andre.helpdesk.services.validation.ValidaCPF;
 import com.andre.helpdesk.services.validation.ValidaEmail;
 
@@ -28,6 +30,20 @@ public class ClienteService {
 		return clientes.stream().map(x -> new ClienteDTO(x)).collect(Collectors.toSet());
 	}
 	
+	public Cliente findById(Long id) {
+		
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		
+		return cliente.orElseThrow(()-> new ObjectNotFoundException("Cliente não encontrado ID : " + id));
+	}
+	
+	public ClienteDTO create(Cliente obj) {
+		obj.setId(null);
+		validaCPF.validaCPF(obj);
+		validaEmail.validaEmail(obj);
+		Cliente cliente = clienteRepository.save(obj);
+		return new ClienteDTO(cliente);
+	}
 	
 	
 }
