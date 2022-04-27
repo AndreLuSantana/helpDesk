df8ea7b5daf8ce0d6355bad1ee4f8ea83834ec45
Andre Santana <andrelusantana@hotmail.com>
2022-04-27 20:09:01 -0300

    Ajustes classe ChamadoDTO

--- src/main/java/com/andre/helpdesk/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/andre/helpdesk/domain/dtos/ChamadoDTO.java
@@ -25,11 +25,14 @@ public class ChamadoDTO implements Serializable {
 	private String observacoes;
 	private Cliente cliente;
 	private Tecnico tecnico;
-	
+	private String nomeCliente;
+	private String nomeTecnico;
+
 	public ChamadoDTO() {
 		super();
-		
+
 	}
+
 	public ChamadoDTO(Chamado chamado) {
 		super();
 		this.id = chamado.getId();
@@ -41,60 +44,96 @@ public class ChamadoDTO implements Serializable {
 		this.observacoes = chamado.getObservacoes();
 		this.cliente = chamado.getCliente();
 		this.tecnico = chamado.getTecnico();
+		this.nomeCliente = chamado.getCliente().getNome();
+		this.nomeTecnico = chamado.getTecnico().getNome();
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public LocalDate getDataAbertura() {
 		return dataAbertura;
 	}
+
 	public void setDataAbertura(LocalDate dataAbertura) {
 		this.dataAbertura = dataAbertura;
 	}
+
 	public LocalDate getDataFechameno() {
 		return dataFechameno;
 	}
+
 	public void setDataFechameno(LocalDate dataFechameno) {
 		this.dataFechameno = dataFechameno;
 	}
+
 	public Prioridade getPrioridade() {
 		return prioridade;
 	}
+
 	public void setPrioridade(Prioridade prioridade) {
 		this.prioridade = prioridade;
 	}
+
 	public Status getStatus() {
 		return status;
 	}
+
 	public void setStatus(Status status) {
 		this.status = status;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getObservacoes() {
 		return observacoes;
 	}
+
 	public void setObservacoes(String observacoes) {
 		this.observacoes = observacoes;
 	}
+
 	public Cliente getCliente() {
 		return cliente;
 	}
+
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+
 	public Tecnico getTecnico() {
 		return tecnico;
 	}
+
 	public void setTecnico(Tecnico tecnico) {
 		this.tecnico = tecnico;
 	}
-	
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getNomeTecnico() {
+		return nomeTecnico;
+	}
+
+	public void setNomeTecnico(String nomeTecnico) {
+		this.nomeTecnico = nomeTecnico;
+	}
+
 }
