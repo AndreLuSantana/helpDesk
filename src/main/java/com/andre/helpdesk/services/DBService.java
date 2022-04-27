bbeb76a6adf27f259d804de42460e262198d55b1
Andre Santana <andrelusantana@hotmail.com>
2022-04-27 14:28:55 -0300

    Criação de mais uma instância de cliente para testes

--- src/main/java/com/andre/helpdesk/services/DBService.java
+++ src/main/java/com/andre/helpdesk/services/DBService.java
@@ -33,12 +33,14 @@ public void instanciaDB() {
 		Tecnico tec2 = new Tecnico(null, "André Santana", "817.602.482-15", "andre@mail.com", "123");
 		
 		Cliente cli1 = new Cliente(null, "Linus Torvalds", "718.881.310-80", "torvalds@mail.com", "123");
+		Cliente cli2 = new Cliente(null, "Karleane Cleica Lobato de Vasconcelos", "062.201.190-10", "nane@mail.com", "123");
 		
 		Chamado cha1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO,"Chamdo 01", "Primeiro chamado", cli1, tec1);
 		
 		tecnicoRepository.saveAll(Arrays.asList(tec1));
 		tecnicoRepository.saveAll(Arrays.asList(tec2));
 		clienteRepository.saveAll(Arrays.asList(cli1));
+		clienteRepository.saveAll(Arrays.asList(cli2));
 		chamadoRepository.saveAll(Arrays.asList(cha1));
 	}
 }
