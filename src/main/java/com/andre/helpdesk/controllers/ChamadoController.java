4919d01e975694d6a6dd4077da592516aadc83dd
Andre Santana <andrelusantana@hotmail.com>
2022-04-28 10:09:27 -0300

    Criação método create ChamadoController

--- src/main/java/com/andre/helpdesk/controllers/ChamadoController.java
+++ src/main/java/com/andre/helpdesk/controllers/ChamadoController.java
@@ -1,13 +1,19 @@
 package com.andre.helpdesk.controllers;
 
+import java.net.URI;
 import java.util.Set;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andre.helpdesk.domain.Chamado;
 import com.andre.helpdesk.domain.dtos.ChamadoDTO;
@@ -30,5 +36,12 @@ public class ChamadoController {
 	public ResponseEntity<Set<ChamadoDTO>> findAll(){
 		Set<ChamadoDTO> chamadoDTO = chamadoService.findAll();
 		return ResponseEntity.ok().body(chamadoDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO chamadoDTO){
+		Chamado chamado = chamadoService.create(chamadoDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(chamado).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
