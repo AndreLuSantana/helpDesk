727ef38973aa9e445568fef634f456df7a5fbbae
Andre Santana <andrelusantana@hotmail.com>
2022-04-28 06:41:17 -0300

    Criação método findAll ChamadoController

--- src/main/java/com/andre/helpdesk/controllers/ChamadoController.java
+++ src/main/java/com/andre/helpdesk/controllers/ChamadoController.java
@@ -1,14 +1,20 @@
 package com.andre.helpdesk.controllers;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.andre.helpdesk.domain.Chamado;
 import com.andre.helpdesk.domain.dtos.ChamadoDTO;
 import com.andre.helpdesk.services.ChamadoService;
 
+@RestController
+@RequestMapping(value = "/chamados")
 public class ChamadoController {
 
 	@Autowired
@@ -18,5 +24,11 @@ public class ChamadoController {
 	public ResponseEntity<ChamadoDTO> findById(@PathVariable Long id){
 		Chamado chamado = chamadoService.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(chamado));
+	}
+	
+	@GetMapping
+	public ResponseEntity<Set<ChamadoDTO>> findAll(){
+		Set<ChamadoDTO> chamadoDTO = chamadoService.findAll();
+		return ResponseEntity.ok().body(chamadoDTO);
 	}
 }
