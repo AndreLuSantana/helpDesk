09991b5a331d760958351eb67a33e1868aaee995
Andre Santana <andrelusantana@hotmail.com>
2022-04-28 06:39:53 -0300

    Criação método findAll ChamadoService

--- src/main/java/com/andre/helpdesk/services/ChamadoService.java
+++ src/main/java/com/andre/helpdesk/services/ChamadoService.java
@@ -1,11 +1,15 @@
 package com.andre.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andre.helpdesk.domain.Chamado;
+import com.andre.helpdesk.domain.dtos.ChamadoDTO;
 import com.andre.helpdesk.repositories.ChamadoRepository;
 import com.andre.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -18,5 +22,10 @@ public class ChamadoService {
 	public Chamado findById(Long id) {
 		Optional<Chamado> chamado = chamadoRepository.findById(id);
 		return chamado.orElseThrow(()-> new ObjectNotFoundException("Chamado não encontrado."));
+	}
+
+	public Set<ChamadoDTO> findAll() {
+		List<Chamado> chamados = chamadoRepository.findAll();
+		return chamados.stream().map(x -> new ChamadoDTO(x)).collect(Collectors.toSet());
 	}
 }
